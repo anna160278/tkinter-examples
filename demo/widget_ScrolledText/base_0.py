4f967d0949126baf32f470d3da0bb0aedf963035
唐钰豆豆 <doudoudzj@qq.com>
2020-12-08 00:45:01 +0800

    update

--- demo/widget_ScrolledText/base_0.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-'''极简模式'''
-
-from tkinter.scrolledtext import ScrolledText
-
-txt = ScrolledText()
-txt.pack(fill='both', side='left', expand=True)
-txt.mainloop()
